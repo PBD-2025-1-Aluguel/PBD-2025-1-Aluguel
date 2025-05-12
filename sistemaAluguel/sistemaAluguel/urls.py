c584c7ebcba09327825df1b72ef0eb674882d246
AlissonGRN <alissonnascimento.dev@gmail.com>
2025-05-12 19:04:01 -0300

    adcionar rotas para login e register, adcionar funcionalidade de troca de cor dos botões da home

--- sistemaAluguel/sistemaAluguel/urls.py
+++ sistemaAluguel/sistemaAluguel/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from sistemaAluguel.views import HomeView
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('register/', views.RegistroView.as_view(), name='register'),
 ]
--- sistemaAluguel/sistemaAluguel/views.py
+++ sistemaAluguel/sistemaAluguel/views.py
@@ -1,4 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class RegistroView(CreateView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = '/login/'
+
+    def form_valid(self, form):
+        form.save()
+        return redirect(self.success_url)
