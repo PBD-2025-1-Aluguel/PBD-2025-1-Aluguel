bfc241bcc8f81aa02a68a6de31cdc04db47b6335
AlissonGRN <alissonnascimento.dev@gmail.com>
2025-05-22 10:56:27 -0300

    refatorar projeto para ficar mais organizado

--- sistemaAluguel/sistemaAluguel/urls.py
+++ sistemaAluguel/sistemaAluguel/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from sistemaAluguel.views import HomeView
-from django.contrib.auth import views as auth_views
-from . import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('register/', views.RegistroView.as_view(), name='register'),
+    path('', include('aluguel.urls'))
+    
 ]
